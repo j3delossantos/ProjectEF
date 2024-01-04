f1f66e90187de9920307d0a89f9d510492d52a35
Juan III De los santos <j3delossantos@outlook.com>
2024-01-03 23:44:36 -0600

    Added multiple endoints to the api for the CRUD (PUT,PODT,DELET)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using projectef;
+using projectef.models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,57 @@ app.MapGet("/dbconnection", async ([FromServices] TasksContext dbContext) =>
     
     
 
+});
+
+app.MapGet("/api/tasks", async ([FromServices] TasksContext dbContext)=>
+{
+   //return Results.Ok(dbContext.Tasks.Include(p=> p.Category).Where(p=> p.TaskPriority == Priority.Hight));
+   return Results.Ok(dbContext.Tasks.Include(p=> p.Category));
+});
+
+app.MapPost("/api/tasks", async ([FromServices] TasksContext dbContext, [FromBody] projectef.models.Task task)=>
+{
+   task.TaskID = Guid.NewGuid();
+   task.CreationDate = DateTime.Now;
+   await dbContext.AddAsync(task);
+   await dbContext.SaveChangesAsync();
+   return Results.Ok();
+});
+
+app.MapPut("/api/tasks/{id}", async ([FromServices] TasksContext dbContext, [FromBody] projectef.models.Task task, [FromRoute] Guid id)=>
+{
+   var currentTask =dbContext.Tasks.Find(id);
+
+   if (currentTask != null)
+   {
+      currentTask.CategoryID = task.CategoryID;
+      currentTask.Title = task.Title;
+      currentTask.TaskPriority = task.TaskPriority;
+      currentTask.Description = task.Description;
+
+      await dbContext.SaveChangesAsync();
+      return Results.Ok();
+   }
+   else{
+      return Results.NotFound();
+   }
+
+   
+});
+
+app.MapDelete("/api/tasks/{id}", async ([FromServices] TasksContext dbContext, [FromRoute] Guid id)=>
+{
+   var currentTask =dbContext.Tasks.Find(id);
+
+   if (currentTask != null)
+   {
+      dbContext.Remove(currentTask);
+      await dbContext.SaveChangesAsync();
+      return Results.Ok();
+
+   } else{
+      return Results.NotFound();
+   }
 });
 
 app.Run();
